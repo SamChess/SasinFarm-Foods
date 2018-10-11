5ff1e8697dbf5c136be3a4e1a33279730b25bcd2
fernandez kariuki <fcbsammymulwa@gmail.com>
2018-10-11 09:56:55 +0300

    pagination

--- web/buy_products.php
+++ web/buy_products.php
@@ -107,9 +107,66 @@ if(isset($_SESSION['user_firstname'])){
 			<?php
 // Include the database configuration file
 include 'connection.php';
-                   
+   	$query=mysqli_query($connection,"select count(id) from `posts`");
+	$row = mysqli_fetch_row($query);
+ 
+	$rows = $row[0];
+ 
+	$page_rows = 6;
+ 
+	$last = ceil($rows/$page_rows);
+ 
+	if($last < 1){
+		$last = 1;
+	}
+ 
+	$pagenum = 1;
+ 
+	if(isset($_GET['pn'])){
+		$pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
+	}
+ 
+	if ($pagenum < 1) { 
+		$pagenum = 1; 
+	} 
+	else if ($pagenum > $last) { 
+		$pagenum = $last; 
+	}
+ 
+	$limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
+ 
+ 
+	$paginationCtrls = '';
+ 
+	if($last != 1){
+ 
+	if ($pagenum > 1) {
+        $previous = $pagenum - 1;
+		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$previous.'" class="btn btn-default">Previous</a> &nbsp; &nbsp; ';
+ 
+		for($i = $pagenum-4; $i < $pagenum; $i++){
+			if($i > 0){
+		        $paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-default">'.$i.'</a> &nbsp; ';
+			}
+	    }
+    }
+ 
+	$paginationCtrls .= ''.$pagenum.' &nbsp; ';
+ 
+	for($i = $pagenum+1; $i <= $last; $i++){
+		$paginationCtrls .= '<a href="'.$_SERVER['PHP_SELF'].'?pn='.$i.'" class="btn btn-default">'.$i.'</a> &nbsp; ';
+		if($i >= $pagenum+4){
+			break;
+		}
+	}
+ 
+    if ($pagenum != $last) {
+        $next = $pagenum + 1;
+        $paginationCtrls .= ' &nbsp; &nbsp; <a href="'.$_SERVER['PHP_SELF'].'?pn='.$next.'" class="btn btn-default">Next</a> ';
+    }
+	}
 // Get images from the database
-$query = $connection->query("SELECT fileName,product,description FROM posts");
+$query = $connection->query("SELECT fileName,product,description FROM posts $limit");
 
 if($query->num_rows > 0){
     while($row = $query->fetch_assoc()){
@@ -130,13 +187,17 @@ if($query->num_rows > 0){
 			</div>'
 
            ?>
+
 <?php }
 }else{ ?>
     <p>No image(s) found...</p>
 <?php } ?>
+
 			
 		</div>
+<div class="pagination_controls"><?php echo $paginationCtrls; ?></div>
 	    </div>
+	 
 		<!-- /.row -->
 		<!-- footer -->
 		<div class="footer">
